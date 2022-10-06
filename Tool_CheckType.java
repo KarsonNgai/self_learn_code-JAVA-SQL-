9abd766a44ab86f5f3ff3ad39a35bdd8fa97808b
Karson Ngai <104995898+KarsonNgai@users.noreply.github.com>
2022-10-06 11:29:55 +0800

    Create Tool_CheckType.java

---
+++ Tool_CheckType.java
@@ -0,0 +1,20 @@
+class Typetester {
+    void printType(byte x) {
+        System.out.println(x + " is an byte");
+    }
+    void printType(int x) {
+        System.out.println(x + " is an int");
+    }
+    void printType(float x) {
+        System.out.println(x + " is an float");
+    }
+    void printType(double x) {
+        System.out.println(x + " is an double");
+    }
+    void printType(char x) {
+        System.out.println(x + " is an char");
+    }
+    void PrintType(Object x) {
+        System.out.println(x + " is an object");
+    }
+}
