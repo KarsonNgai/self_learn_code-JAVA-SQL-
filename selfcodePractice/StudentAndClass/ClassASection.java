86faea0c5e4b921041f0f20cc88398583aea6583
Karson Ngai <104995898+KarsonNgai@users.noreply.github.com>
2022-10-15 13:57:27 +0800

    Create ClassASection.java

---
+++ selfcodePractice/StudentAndClass/ClassASection.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class ClassASection extends ClassSection{
+  //public static ArrayList<Student> studentArrs = new ArrayList<>();
+    //冇用
+    
+  ClassASection(){
+    super.setFood("apple");
+    super.setIsBoy(true);
+  }
+  //for ClassA use, considering that there may have other section.
+}
