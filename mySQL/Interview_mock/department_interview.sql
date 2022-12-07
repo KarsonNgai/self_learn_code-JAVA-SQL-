f3cf3185f4a21460dcbe318deeccbeea5adbdd3d
Karson Ngai <104995898+KarsonNgai@users.noreply.github.com>
2022-12-07 11:09:35 +0800

    Update and rename mySQL/ExerciseAboutEmployee/department_interview.sql to mySQL/Interview_mock/department_interview.sql

--- mySQL/Interview_mock/department_interview.sql
+++ mySQL/Interview_mock/department_interview.sql
@@ -33,12 +33,18 @@ INSERT INTO EMPLOYEE VALUES (4, 'SUNNY', 40000, 90234563, 'SUNNY@GMAIL.COM', 4);
 -- if the number of employees are same, then order department code alphabetically
 -- select 2 column in total: 'Department Code' and 'number of employees'
 
-select 
-  d.dept_code as 'Department Code', 
-  count(*) as 'number of employees' 
-from department as d
-right join employee as e 
+-- expected:
+-- Department Code | number of employees
+-- HR |2
+-- IT |1
+-- SA |1
+-- 9upper|0 <<this would be one of the challenge point
+
+select d.dept_code as 'Department Code', count(e.dept_id) as 'number of employee' from department as d
+left join employee as e 
 on d.id = e.dept_id
 group by d.id
-order by 'number of employees' desc, d.dept_code;
+order by count(e.dept_id) desc, d.dept_code;
+
+
 
