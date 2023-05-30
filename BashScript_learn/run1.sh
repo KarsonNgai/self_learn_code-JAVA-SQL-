897c3da0e7ef8ccd565e4a2930be9552b71bd0ba
Karson Ngai <104995898+KarsonNgai@users.noreply.github.com>
2023-05-30 16:28:56 +0800

    Update run1.sh

--- BashScript_learn/run1.sh
+++ BashScript_learn/run1.sh
@@ -4,6 +4,13 @@ set timeout -1
 
 spawn git status
 
+expect "On branch main"
+expect "Your branch is up to date with 'origin/main'."
+
+expect "nothing to commit, working tree clean"
+
+send -- "the branch is update to date"
+
 spawn git pull
 
 expect eof
